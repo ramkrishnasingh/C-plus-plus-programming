f9ea4bc2634fe3b8d7f5d926ab80e975680aa65f
Ram Krishna Singh <30290728+codeitram@users.noreply.github.com>
2019-07-07 21:24:33 +0530

    int_fast32_t || review

--- cstdint.cpp
+++ cstdint.cpp
@@ -5,9 +5,10 @@ int main(int argc, char const *argv[])
 {
 	//this is used to represent char or int so avoid
 	//using it so we can use int16_t or int_fast32_t
-	std::int8_t i(65);
+	std::int8_t i(65);    // will print here int value
 //int_fast32_t favours performance 
 	//int_least32_t will favour memory conservation
 	std::cout<<i<<std::endl;
 	return 0;
+	
 }
